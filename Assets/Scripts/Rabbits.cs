9c38e2c5c9cef93719b8d7f86e73c17e6ecc0b77
Oslik18 <oslik18@gmail.com>
2023-05-14 20:13:24 +1000

    jump_rabbits

---
+++ Assets/Scripts/Rabbits.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbits : MonoBehaviour
+{
+    private Vector3 movement;
+    public Animator anim;
+    public Transform[] waypoints;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
